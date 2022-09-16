8f1391245fc0bd09ee09f6f56f4afd78ad4a67fd
SanjanaCode <sanjanareddy.feb2001@gmail.com>
2022-09-16 00:03:33 -0700

    added an inline comment

--- SortArray.java
+++ SortArray.java
@@ -28,7 +28,7 @@ public class SortArray {
     public static String[] sortString(String arr[]) {
 		for(int i=0;i<arr.length;i++) {
 			for(int j=0; j<arr.length-1-i;j++) {
-			if(arr[j].compareToIgnoreCase(arr[j+1])>0) {	
+			if(arr[j].compareToIgnoreCase(arr[j+1])>0) {	//compares two strings lexicographically, irrespective of the case
 				String temp=arr[j];
 				arr[j]=arr[j+1];
 				arr[j+1]=temp;
